402b048d264f6a892b2d18ee57e938ca9965d937
Ratiarivelo Nekena Rayane <50442872+Online13@users.noreply.github.com>
2023-12-21 07:39:14 +0300

    Create Timer.py
    Timer is responsible to evaluate the duration of some task

---
+++ app/Timer.py
@@ -0,0 +1,28 @@
+import time
+
+class Timer:
+    def __init__(self):
+        self.start_time = None
+        self.end_time = None
+
+    def start(self):
+        """Starts the timer."""
+        self.start_time = time.perf_counter_ns()  # Use perf_counter_ns for high precision
+
+    def stop(self):
+        """Stops the timer and returns the elapsed time in seconds."""
+        if self.start_time is None:
+          raise RuntimeError("Timer is not running")
+        self.end_time = time.perf_counter_ns()
+        return (self.end_time - self.start_time) / 1e9  # Convert nanoseconds to seconds
+
+    def __enter__(self):
+        """Starts the timer when entering a `with` block."""
+        self.start()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """Stops the timer and optionally prints the elapsed time when exiting a `with` block."""
+        elapsed_time = self.stop()
+        if exc_type is None:
+            print(f"Elapsed time: {elapsed_time:.6f} seconds")
